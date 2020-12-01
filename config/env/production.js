351d805fb57a5eda790aaaccb29f2f40ea44c682
stabishali8 <stabishali8@gmail.com>
2020-12-01 18:19:04 +0500

    heroku changes

--- config/env/production.js
+++ config/env/production.js
@@ -12,7 +12,7 @@
  *
  * > If you're using git as a version control solution for your Sails app,
  * > this file WILL BE COMMITTED to your repository by default, unless you add
- * > it to your .gitignore file.  If your repository will be publicly viewable,
+ * > it to your .gitignore file.  If your repository will be publiclyt viewable,
  * > don't add private/sensitive data (like API secrets / db passwords) to this file!
  *
  * For more best practices and tips, see:
@@ -49,28 +49,7 @@ module.exports = {
     default: {
       // adapter: 'sails-mysql',
       // url: 'mysql://user:password@host:port/database',
-      //--------------------------------------------------------------------------
-      //  /\   To avoid checking it in to version control, you might opt to set
-      //  ||   sensitive credentials like `url` using an environment variable.
-      //
-      //  For example:
-      //  ```
-      //  sails_datastores__default__url=mysql://admin:myc00lpAssw2D@db.example.com:3306/my_prod_db
-      //  ```
-      //--------------------------------------------------------------------------
-
-      /****************************************************************************
-      *                                                                           *
-      * More adapter-specific options                                             *
-      *                                                                           *
-      * > For example, for some hosted PostgreSQL providers (like Heroku), the    *
-      * > extra `ssl: true` option is mandatory and must be provided.             *
-      *                                                                           *
-      * More info:                                                                *
-      * https://sailsjs.com/config/datastores                                     *
-      *                                                                           *
-      ****************************************************************************/
-      // ssl: true,
+      ssl: false,
 
     },
 
